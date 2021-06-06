2de803facf40c2dd70c50452ae385b8b288a99f3
arnab-bit <rnbpttnk@gmail.com>
2021-06-06 23:20:36 +0530

    create registration form field and validators with reactive formgroup

--- frontend/src/app/user/user-registration/user-registration.component.ts
+++ frontend/src/app/user/user-registration/user-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-registration',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-registration.component.css']
 })
 export class UserRegistrationComponent implements OnInit {
-
+  registrationForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.registrationForm = new FormGroup({
+      userName: new FormControl('username', Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+      confirmPassword: new FormControl(null, Validators.required),
+      mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
+    });
+  }
+
+  onSubmit(){
+    console.log(this.registrationForm);
   }
 
 }
